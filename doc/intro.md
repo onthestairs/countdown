364f5176729b78dedd73876d8f9d0b00287b89f2
Austin Platt <austinplatt@gmail.com>
2013-09-02 18:20:04 +0100

    first working version

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to countdown-simon
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/countdown_simon/core_test.clj
@@ -0,0 +1,7 @@
+(ns countdown-simon.core-test
+  (:require [clojure.test :refer :all]
+            [countdown-simon.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
