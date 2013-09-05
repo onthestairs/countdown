e63b07d64e0f7ee46a9703441addbf38458d5a90
Austin Platt <austinplatt@gmail.com>
2013-09-05 22:46:56 +0100

    tidy up

--- src/countdown_simon/core.clj
+++ src/countdown_simon/core.clj
@@ -3,29 +3,38 @@
 (def operators
   ['+ '- '* '/])
 
-(defn locate-solutions [target ns]
-  (let [solutions (find-solutions target (map (fn [n]
-                                                 {:value n :exp n}) ns))]
-    (map (fn [solution]
-           (:exp solution)) solutions)))
-
-(defn print [coll]
-  (map (fn [item]
-         (println item)) coll))
-
-(defn find-solutions [target ns]
-  (let [evaled-solutions (eval-solutions target ns)
-        new-solutions (if (> (count ns) 1)
-                        (reduce-pairs target ns)
-                        `())
-        all-solutions (concat evaled-solutions new-solutions)]
-    all-solutions))
-
 (defn eval-solutions [target ns]
   (filter (fn [n]
             (= target (:value n))) ns))
 
-(defn reduce-pairs [target ns]
+(defn should-compute? [operator a b]
+  (cond
+   (= operator '+) (> a b)
+   (= operator '*) (> a b)
+   (= operator '/) (and (not= b 0)
+                       (= (mod a b) 0))
+   :else true))
+
+(defn coll-rotations [coll]
+  (let [count (count coll)]
+    (map (fn [n]
+           (concat
+            (take-last (- count n) coll)
+            (take n coll))) (range count))))
+
+(defn pairs [ns]
+  (let [rotations (coll-rotations ns)]
+    (flatten (concat (map (fn [rotation]
+                            (let [a (first rotation)
+                                  remaining (rest rotation)
+                                  rotations-2 (coll-rotations remaining)]
+                              (map (fn [rotation-2]
+                                     (let [b (first rotation-2)
+                                           rest-2 (rest rotation-2)]
+                                       {:pair [a b]
+                                        :rest rest-2})) rotations-2))) rotations)))))
+
+(defn contract-pairs [target ns]
   (let [pair-and-rests (pairs ns)]
     (mapcat (fn [pair-and-rest]
               (let [pair (:pair pair-and-rest)
@@ -42,29 +51,16 @@
                                            '())) operators)]
                 solutionss)) pair-and-rests)))
 
-(defn should-compute? [operator a b]
-  (cond
-   (= operator '+) (> a b)
-   (= operator '*) (> a b)
-   (= operator '/) (and (not= b 0) (= (mod a b) 0))
-   :else true))
+(defn find-solutions [target ns]
+  (let [evaled-solutions (eval-solutions target ns)
+        new-solutions (if (> (count ns) 1)
+                        (contract-pairs target ns)
+                        `())
+        all-solutions (concat evaled-solutions new-solutions)]
+    all-solutions))
 
-(defn coll-rotations [coll]
-  (let [count (count coll)]
-    (map (fn [n]
-           (concat
-            (take-last (- count n) coll)
-            (take n coll))) (range count)))))
-
-(defn pairs [ns]
-  (let [rotations (coll-rotations ns)]
-    (flatten (concat (map (fn [rotation]
-                            (let [a (first rotation)
-                                  remaining (rest rotation)
-                                  rotations-2 (coll-rotations remaining)]
-                              (map (fn [rotation-2]
-                                     (let [b (first rotation-2)
-                                           rest-2 (rest rotation-2)]
-                                       {:pair [a b]
-                                        :rest rest-2})) rotations-2))) rotations)))))
-
+(defn locate-solutions [target ns]
+  (let [solutions (find-solutions target (map (fn [n]
+                                                 {:value n :exp n}) ns))]
+    (map (fn [solution]
+           (:exp solution)) solutions)))
